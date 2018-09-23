d339ae8a752343ea2ed50a78657e30b230e53e6f
Tom Barezer <toba1020@berkeley.edu>
2018-09-22 23:57:58 -0700

    Vector now has insert at index and remove at index methods

--- Vector.h
+++ Vector.h
@@ -24,6 +24,8 @@ private:
    Node** m_arr;
 
 public:
+   typedef Iterator<Node*, Type> iterator;
+
    Vector(const size_t capacity = 10);
    ~Vector();
 
@@ -32,11 +34,13 @@ public:
    size_t size() const { return m_size; }
    size_t capacity() const { return m_capacity; }
 
+   void insert(const Type & data, const size_t ind);
+   void remove(const size_t ind);
    void appendToEnd(const Type & data);
    Type & operator[](const size_t i);
 
-    Iterator<Node*, Type> begin() { return Iterator<Node*, Type>(m_arr); }
-    Iterator<Node*, Type> end() { return Iterator<Node*, Type>(m_arr + m_size); }
+   iterator begin() { return iterator(m_arr); }
+   iterator end() { return iterator(m_arr + m_size); }
 };
 
 template <class Type>
@@ -65,9 +69,53 @@ void Vector<Type>::resize(const size_t capacity)
    for (size_t i = 0; i < end; i++)
       tempArr[i] = m_arr[i];
 
+   for (size_t i = end; i < m_size; i++)
+   {
+      delete m_arr[i];
+      m_arr[i] = NULL;
+   }
+
    delete[] m_arr;
    m_arr = NULL;
    m_arr = tempArr;
+}
+
+template <class Type>
+void Vector<Type>::insert(const Type & data, const size_t ind)
+{
+   if (m_size == m_capacity)
+      resize(m_capacity * 2);
+   if (ind >= m_size)
+   {
+      appendToEnd(data);
+      return;
+   }
+
+   for (size_t i = m_size; i > ind; i--)
+      m_arr[i] = m_arr[i - 1];
+
+   m_arr[ind] = new Node(data);
+
+   ++m_size;
+}
+
+template <class Type>
+void Vector<Type>::remove(const size_t ind)
+{
+   if (m_size == 0)
+      return;
+   if (m_size == m_capacity / 2 - 1)
+      resize(m_capacity / 2);
+
+   if (ind < m_size)
+      delete m_arr[ind];
+   else
+      delete m_arr[m_size];
+
+   --m_size;
+
+   for (size_t i = ind; i < m_size; i++)
+      m_arr[i] = m_arr[i + 1];
 }
 
 template <class Type>
@@ -84,7 +132,7 @@ Type & Vector<Type>::operator[](const size_t i)
    if (i > m_size)
    {
       std::cout << "i must be LESS than " << m_size << std::endl;
-      return **(m_arr[0]);
+      return **(m_arr[m_size - 1]);
    }
    return **(m_arr[i]);
 }
