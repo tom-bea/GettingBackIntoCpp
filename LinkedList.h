1813ca1e77d37507c41205d31edd67de3c881312
Tom Bar Ezer Ayalon <tomtom.barezer@gmail.com>
2018-10-14 16:19:27 -0700

    linked list and vector no longer inherit from collection. tried implenting copy constructor for vector.

--- LinkedList.h
+++ LinkedList.h
@@ -1,53 +1,74 @@
 #pragma once
 
-#include "Collection.h"
-
 template <class Type>
-class LinkedList : Collection<Type>
+class LinkedList
 {
 private:
-   typedef Collection<Type>::iterator iterator;
-
-   class Node : public Collection<Type>::Node
-   {
-   private:
-      Node* m_next;
-
-   public:
-      Node(const Type& data, Node* next = nullptr) : Collection<Type>::Node(data), m_next(next) {}
-      Node(Node* other) : Node(**other, other->m_next) {}
-
-      Node& operator++(int)
-      {
-         Node* ret = this;
-         ++*this;
-         return *ret;
-      }
-      Node& operator++()
-      {
-         ;
-         return *this;
-      }
-
-      Node* next() const { return m_next; }
-   };
+   class Node;
+   class Iterator;
 
    size_t m_size;
-   Node* m_head;
+   Node* m_head, m_tail;
 
 public:
    LinkedList();
+   LinkedList(const LinkedList& other);
    ~LinkedList();
 
-   void insert(const Type& data);
+   size_t size() const { return m_size; }
+
    void insert(const Type& data, const size_t ind);
    void insertAtHead(const Type& data);
    void insertAtTail(const Type& data);
+   void remove(const size_t ind);
    void clear();
    Type & operator[](const size_t i);
 
-   iterator begin() { return iterator(nullptr); }
-   iterator end() { return iterator(nullptr); }
+   Iterator begin() { return Iterator(m_head); }
+   Iterator end() { return Iterator(nullptr); }
+};
+
+template <class Type>
+class LinkedList<Type>::Node
+{
+private:
+   Type m_data;
+   Node* m_prev;
+   Node* m_next;
+
+public:
+   Node(const Type& data, const Node* prev = nullptr, const Node* next = nullptr) : m_data(data), m_prev(prev), m_next(next) {}
+
+   bool operator=(const Type& newData) { m_data = newData; return true; }
+   Type& getData() { return m_data; }
+   Node* getPrev() { return m_prev; }
+   bool setPrev(const Node* newPrev) { m_prev = newPrev; return true; }
+   Node* getNext() { return m_next; }
+   bool setNext(const Node* newNext) { m_next = newNext; return true; }
+};
+
+template <class Type>
+class LinkedList<Type>::Iterator
+{
+private:
+   Node* m_walker;
+
+public:
+   Iterator(Node* start) : m_walker(start) {}
+   Type operator*() const { return (*m_walker)->getData(); }
+   bool operator==(const Iterator& other) const { return m_walker == other.m_walker; }
+   bool operator!=(const Iterator& other) const { return !(*this == other); }
+   Type operator++(int)
+   {
+      Type ret = (*m_walker)->getData();
+      ++*this;
+      return ret;
+   }
+   Iterator& operator++()
+   {
+      m_walker = m_walker->getNext();
+      return *this;
+   }
 };
 
 template <class Type>
@@ -55,18 +76,21 @@ LinkedList<Type>::LinkedList()
 {
    m_size = 0;
    m_head = nullptr;
+   m_tail = nullptr;
+}
+
+template <class Type>
+LinkedList<Type>::LinkedList(const LinkedList<Type>& other)
+{
+   m_size = other.m_size;
+   for (auto x : other)
+      (*this).insertAtTail(x);
 }
 
 template <class Type>
 LinkedList<Type>::~LinkedList()
 {
    clear();
-}
-
-template <class Type>
-void LinkedList<Type>::insert(const Type& data)
-{
-   insertAtHead(data);
 }
 
 template <class Type>
@@ -77,16 +101,22 @@ void LinkedList<Type>::insert(const Type& data, const size_t ind)
    if (ind == 0)
    {
       m_head = new Node(data, m_head);
+      m_head->getNext()->setPrev(m_head);
+      return;
+   }
+   if (ind >= m_size)
+   {
+      m_tail = new Node(data, nullptr, m_tail);
+      m_tail->getPrev()->setNext(m_head);
       return;
    }
 
-   Node* walker = new Node(m_head);
+   Node* walker = m_head;
    for (size_t i = 0; i < ind - 1 && i < m_size - 1; i++)
-      walker = walker->next();
+      walker = walker->getNext();
 
-   walker->next() = new Node(data, walker->next());
-
-   delete walker;
+   walker->setNext(new Node(data, walker, walker->getNext()));
+   walker->getNext()->getNext()->setPrev(walker->getNext());
 }
 
 template <class Type>
@@ -102,10 +132,51 @@ void LinkedList<Type>::insertAtTail(const Type& data)
 }
 
 template <class Type>
+void LinkedList<Type>::remove(const size_t ind)
+{
+   if (m_size == 0)
+      return;
+
+   if (ind == 0)
+   {
+      Node* newHead = m_head->getNext();
+      delete m_head;
+      m_head = newHead;
+      m_head->setPrev(nullptr);
+      --m_size;
+      return;
+   }
+
+   if (ind >= m_size)
+   {
+      Node* newTail = m_tail->getPrev();
+      delete m_tail;
+      m_tail = newTail;
+      m_tail->setNext(nullptr);
+      --m_size;
+      return;
+   }
+
+   Node* prev = m_head;
+   Node* curr = m_head->getNext();
+   for (size_t i = 1; i < ind && i < m_size; i++)
+   {
+      prev = curr;
+      curr = curr->getNext();
+   }
+   prev->setNext(curr->getNext());
+   curr->getNext()->setPrev(prev);
+   delete curr;
+   --m_size;
+}
+
+template <class Type>
 void LinkedList<Type>::clear()
 {
    if (m_size == 0)
       return;
+
+   m_size = 0;
 
    Node* curNode = m_head;
    Node* nextNode = m_head->next();
@@ -129,11 +200,9 @@ Type & LinkedList<Type>::operator[](const size_t i)
       return **m_head;
    }
 
-   Node* walker = new Node(m_head);
+   Node* walker = m_head;
    for (size_t j = 0; j < i; j++)
-      walker = walker->next();
+      walker = walker->getNext();
 
-   Type ret = **walker;
-   delete walker;
-   return ret;
+   return walker->getData();
 }
--- Vector.h
+++ Vector.h
@@ -1,14 +1,13 @@
 #pragma once
 
-#include "Collection.h"
 #include <iostream>
 
 template <class Type>
-class Vector : public Collection<Type>
+class Vector
 {
 private:
-   typedef Collection<Type>::Node Node;
-   typedef Collection<Type>::iterator iterator;
+   class Node;
+   class Iterator;
 
    size_t m_size;
    size_t m_capacity;
@@ -16,7 +15,7 @@ private:
 
 public:
    Vector(const size_t capacity = 10);
-   // TODO: clone constructor
+   Vector(Vector& other);
    ~Vector();
 
    void resize(const size_t capacity);
@@ -24,15 +23,51 @@ public:
    size_t size() const { return m_size; }
    size_t capacity() const { return m_capacity; }
 
-   void insert(const Type& data);
    void insert(const Type & data, const size_t ind);
    void remove(const size_t ind);
    void clear();
    void appendToEnd(const Type & data);
    Type & operator[](const size_t i);
 
-   iterator begin() { return iterator(m_arr); }
-   iterator end() { return iterator(m_arr + m_size); }
+   Iterator begin() { return Iterator(m_arr); }
+   Iterator end() { return Iterator(m_arr + m_size); }
+};
+
+template <class Type>
+class Vector<Type>::Node
+{
+private:
+   Type m_data;
+
+public:
+   Node(const Type& data) : m_data(data) {}
+
+   bool operator=(const Type& newData) { m_data = newData; return true; }
+   Type& getData() { return m_data; }
+};
+
+template <class Type>
+class Vector<Type>::Iterator
+{
+private:
+   Node** m_walker;
+
+public:
+   Iterator(Node** start) : m_walker(start) {}
+   Type operator*() const { return (*m_walker)->getData(); }
+   bool operator==(const Iterator& other) const { return m_walker == other.m_walker; }
+   bool operator!=(const Iterator& other) const { return !(*this == other); }
+   Type operator++(int)
+   {
+      Type ret = (*m_walker)->getData();
+      ++*this;
+      return ret;
+   }
+   Iterator& operator++()
+   {
+      ++m_walker;
+      return *this;
+   }
 };
 
 template <class Type>
@@ -41,6 +76,17 @@ Vector<Type>::Vector(const size_t capacity)
    m_arr = new Node*[capacity];
    m_capacity = capacity;
    m_size = 0;
+}
+
+template <class Type>
+Vector<Type>::Vector(Vector<Type>& other)
+{
+   m_capacity = other.m_capacity;
+   m_size = other.m_size;
+   m_arr = new Node*[m_capacity];
+
+   for (Type x : other)
+      this->appendToEnd(x);
 }
 
 template <class Type>
@@ -56,18 +102,22 @@ void Vector<Type>::resize(const size_t capacity)
    Node** tempArr = new Node*[capacity];
    size_t end = (capacity > m_size) ? m_size : capacity;
    m_capacity = capacity;
+   m_size = 0;
 
    for (size_t i = 0; i < end; i++)
+   {
       tempArr[i] = m_arr[i];
+      m_size++;
+   }
 
    for (size_t i = end; i < m_size; i++)
    {
       delete m_arr[i];
-      m_arr[i] = NULL;
+      m_arr[i] = nullptr;
    }
 
    delete[] m_arr;
-   m_arr = NULL;
+   m_arr = nullptr;
    m_arr = tempArr;
 }
 
@@ -88,14 +138,6 @@ void Vector<Type>::insert(const Type & data, const size_t ind)
    m_arr[ind] = new Node(data);
 
    ++m_size;
-}
-
-template <class Type>
-void Vector<Type>::insert(const Type & data)
-{
-   if (m_size == m_capacity)
-      resize(m_capacity * 2);
-   appendToEnd(data);
 }
 
 template <class Type>
@@ -120,9 +162,9 @@ void Vector<Type>::remove(const size_t ind)
 template <class Type>
 void Vector<Type>::clear()
 {
-   m_size = 0;
    for (size_t i = 0; i < m_size; i++)
       delete m_arr[i];
+   m_size = 0;
 }
 
 template <class Type>
@@ -139,7 +181,7 @@ Type & Vector<Type>::operator[](const size_t i)
    if (i > m_size) //TODO: figure out if there is supposed to be >= or if > is fine
    {
       std::cout << "i must be LESS than " << m_size << std::endl;
-      return **(m_arr[m_size - 1]);
+      return m_arr[m_size - 1]->getData();
    }
-   return **(m_arr[i]);
+   return m_arr[i]->getData();
 }
